a3e6a7be1bc21acca75e76f77d7ee120cfe81229
Zeeshanch123 <zeeshanch160@gmail.com>
2025-07-07 18:58:49 +0500

    chore:port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import getPort from 'get-port';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -30,6 +29,5 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
-  console.log("await getPort()=>>>>>>>>", await getPort());
 }
 bootstrap();
